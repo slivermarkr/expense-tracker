8284a9adb36367be503619457960d825e5c9cd46
sliver <mark.antipas4@gmail.com>
2025-03-31 23:10:57 +0800

    some inp filter

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@
     const initHTML = () => {
         return `
         <div class="balance-container">
-            <p>Balance: $<span class="balance">0.00</span></p>
+            <p>Balance: ₱<span class="balance">0.00</span></p>
         </div>
 
         <div class="profit-expense-container">
             <div class="profit-container">
                 <p class="profit-heading">Profit: </p>
-                <p class="profitWrap">$<span class="profit">0.00</span></p>
+                <p class="profitWrap">₱<span class="profit">0.00</span></p>
             </div>
             <div class="expense-container">
                 <p class="expense-heading">Expense: </p>
-                <p class="expenseWrap">-$<span class="expense">0.00</span></p>
+                <p class="expenseWrap">-₱<span class="expense">0.00</span></p>
             </div>
 
         </div>
@@ -26,15 +26,15 @@
         <form class="myForm">
             <legend>Add New Transaction</legend>
             <p>
-                <label for="inputText">Transaction: </label>
-                <input type="text" placeholder="New Transaction" id="inputText">
+                <label for="inputText">Name: </label>
+                <input type="text" placeholder="New Transaction" id="inputText" required>
             </p>
             <p>
                 <label for="inputAmount">Amount: </label>
-                <input type="text" placeholder="0.00" id="inputAmount">
+                <input type="text" placeholder="0.00" id="inputAmount" required>
             </p>
             <p>
-                <button>Enter</button>
+                <button class="submitBtn">Enter</button>
             </p>
         </form>
         `
@@ -49,16 +49,17 @@
                 ${name}
             </p>
             <p class="transaction-amount">
-                ${amount > 0 ? "$" + amount : "-$" + Math.abs(amount)}
+                ${amount > 0 ? "₱" + amount : "-₱" + Math.abs(amount)}
             </p>
             <p><button class="delete">X</button></p>
         `
         return list;
     }
-    let transactions = [];
+    let transactions = JSON.parse(localStorage.getItem("expense-tracker")) || [];
 
     const updateBalance = (balance) => {
         const balanceContainer = entry.querySelector(".balance");
+        balanceContainer.classList.toggle("expense", balance < 0)
         balanceContainer.textContent = balance.toFixed(2);
     }
     const updateProfit = (profit) => {
@@ -88,10 +89,12 @@
         updateExpense(expense);
         updateBalance(balance);
     }
+
     const addTransaction = (amount, text) => {
         const id = crypto.randomUUID();
         const newTransaction = { id, amount, name: text };
         transactions.push(newTransaction);
+        localStorage.setItem("expense-tracker", JSON.stringify(transactions));
         updateTransaction(transactions)
 
         entry.querySelector(".transactions-list").appendChild(createTransactionItem(newTransaction));
@@ -99,24 +102,49 @@
     const deleteTransaction = (id) => {
         const newArray = transactions.filter(transaction => transaction.id !== id);
         transactions = [...newArray];
+        localStorage.setItem("expense-tracker", JSON.stringify(transactions));
         updateTransaction(transactions)
         entry.querySelector(`.transaction-list--item[data-id="${id}"]`).remove();
     }
 
-    entry.innerHTML = initHTML();
+    const disableBtn = (isDisabled) => {
+        const btn = entry.querySelector(".submitBtn");
+        console.log(isDisabled)
+        if (isDisabled) {
+            btn.disabled = true;
+        } else {
+            btn.disabled = false;
+        }
+    }
 
+    entry.innerHTML = initHTML();
     entry.querySelector(".myForm").addEventListener("submit", e => {
         e.preventDefault();
         const name = entry.querySelector("#inputText").value;
-        const amount = +entry.querySelector("#inputAmount").value;
+        const amount = entry.querySelector("#inputAmount").value;
+        if (isNaN(Number(amount))) {
+            return;
+        };
 
-        addTransaction(amount, name)
+        addTransaction(+amount, name)
         entry.querySelector(".myForm").reset()
+        entry.querySelector("#inputText").focus()
+    })
+
+    entry.querySelector("#inputAmount").addEventListener("input", e => {
+        disableBtn(isNaN(Number(e.target.value)));
     })
 
     entry.addEventListener("click", e => {
         if (!e.target.classList.contains("delete")) return;
-        console.log(e.target.closest("li").dataset.id);
         deleteTransaction(e.target.closest("li").dataset.id)
+    })
+
+    // get data on local storage and update display on initial load.
+    window.addEventListener("load", () => {
+        updateTransaction(transactions)
+        for (const transaction of transactions) {
+            entry.querySelector(".transactions-list").appendChild(createTransactionItem(transaction))
+        }
     })
 })()
