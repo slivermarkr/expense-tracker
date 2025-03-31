b2ce666aa2c339986cfb0592a1f52cb183d918a7
sliver <mark.antipas4@gmail.com>
2025-03-31 22:20:44 +0800

    adding and deletion updates the balance and other display

--- src/app.js
+++ src/app.js
@@ -40,9 +40,10 @@
         `
     }
 
-    const createTransactionItem = (name, amount) => {
+    const createTransactionItem = ({ name, amount, id }) => {
         const list = document.createElement("li")
         list.className = `transaction-list--item ${amount < 0 ? "expense" : "profit"}`;
+        list.setAttribute("data-id", id)
         list.innerHTML = `
             <p class="transaction-name">
                 ${name}
@@ -50,37 +51,56 @@
             <p class="transaction-amount">
                 ${amount > 0 ? "$" + amount : "-$" + Math.abs(amount)}
             </p>
+            <p><button class="delete">X</button></p>
         `
         return list;
     }
-    let currBalance = 0, currProfit = 0, currExpense = 0;
+    let transactions = [];
+
     const updateBalance = (balance) => {
         const balanceContainer = entry.querySelector(".balance");
-        balanceContainer.textContent = balance;
+        balanceContainer.textContent = balance.toFixed(2);
     }
     const updateProfit = (profit) => {
         const profitContainer = entry.querySelector(".profit");
-        profitContainer.textContent = profit;
+        profitContainer.textContent = profit.toFixed(2);
     }
     const updateExpense = (expense) => {
         const expenseContainer = entry.querySelector(".expense");
-        expenseContainer.textContent = Math.abs(expense);
+        expenseContainer.textContent = Math.abs(expense).toFixed(2);
     }
 
-    const updateDigits = (amount) => {
-        if (amount > 0) {
-            //profit
-            currProfit += amount
-            updateProfit(currProfit)
-        } else {
-            //expense
-            console.log(amount)
-            currExpense += amount
-            updateExpense(currExpense)
-        }
-        currBalance = currProfit + currExpense;
-        console.log(currBalance, currProfit, currExpense)
-        updateBalance(currBalance);
+    const updateTransaction = (newTransactionsArray) => {
+        const profit = newTransactionsArray.reduce((acc, curr) => {
+            if (curr.amount && curr.amount > 0) {
+                acc += curr.amount
+            }
+            return acc;
+        }, 0);
+        const expense = newTransactionsArray.reduce((acc, curr) => {
+            if (curr.amount && curr.amount < 0) {
+                acc += curr.amount
+            }
+            return acc;
+        }, 0);
+        const balance = profit + expense;
+        updateProfit(profit);
+        updateExpense(expense);
+        updateBalance(balance);
+    }
+    const addTransaction = (amount, text) => {
+        const id = crypto.randomUUID();
+        const newTransaction = { id, amount, name: text };
+        transactions.push(newTransaction);
+        updateTransaction(transactions)
+
+        entry.querySelector(".transactions-list").appendChild(createTransactionItem(newTransaction));
+    }
+    const deleteTransaction = (id) => {
+        const newArray = transactions.filter(transaction => transaction.id !== id);
+        transactions = [...newArray];
+        updateTransaction(transactions)
+        entry.querySelector(`.transaction-list--item[data-id="${id}"]`).remove();
     }
 
     entry.innerHTML = initHTML();
@@ -90,9 +110,13 @@
         const name = entry.querySelector("#inputText").value;
         const amount = +entry.querySelector("#inputAmount").value;
 
-        entry.querySelector(".transactions-list").appendChild(createTransactionItem(name, amount))
-        updateDigits(amount)
-
+        addTransaction(amount, name)
         entry.querySelector(".myForm").reset()
+    })
+
+    entry.addEventListener("click", e => {
+        if (!e.target.classList.contains("delete")) return;
+        console.log(e.target.closest("li").dataset.id);
+        deleteTransaction(e.target.closest("li").dataset.id)
     })
 })()
